348663e613b4b8f5f65587effaf4a8bc19dfb326
veroxcrown <eldav.corona@gmail.com>
2021-02-20 15:07:33 -0600

    Arreglos validacion login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -35,22 +35,24 @@ function Login() {
     setCredential({ ...credential, [event.target.name]: event.target.value })
   }
 
-  const authHandler = () => { 
-    fetch(endpoint, {
-      method: 'POST',
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(credential),
-    }).then(res => res.json())
-      .catch(error => setStatusAuth(authAlert))
-      .then(response => {
-        if (response.success) {
-          localStorage.setItem("neojwt", response.data.token)
-          window.location.reload()
-          return
-        }
-        setStatusAuth(authAlert)
-        setCredential({password: ""})
-      })
+  const authHandler = () => {
+
+    if (Object.keys(credential).length === 2) {
+      fetch(endpoint, {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(credential),
+      }).then(res => res.json())
+        .catch(error => setStatusAuth(authAlert))
+        .then(response => {
+          if (response.success) {
+            localStorage.setItem("neojwt", response.data.token)
+            window.location.reload()
+            return
+          }
+          setStatusAuth(authAlert)
+        })
+    }
   }
 
   const handlerSubmit = event => {
@@ -64,19 +66,19 @@ function Login() {
         <div class="bg-image"></div>
         <Col sm="12" md={{ size: 8, offset: 2 }} className="d-flex flex-column justify-content-center align-items-center login-col">
           <div className="login rounded d-flex flex-column justify-content-center align-items-center shadow px-4 py-3">
-            <img src={logo} className="m-4" alt="Neo Educate" />
+            <img src={logo} className="m-2 mt-3" alt="Neo Educate" />
             <Form onSubmit={handlerSubmit}>
               <FormGroup className="w-100 m-2 text-brand">
-                <Label>Correo electrónico</Label>
-                <Input type="email" className={`form-control ${statusAuth}`} placeholder="john@neoeducate.com" name="email" onChange={credentialHandler}/>
+                <Label>E-mail</Label>
+                <Input type="email" className={`form-control ${statusAuth}`} placeholder="john@neoeducate.com" name="email" onChange={credentialHandler} required />
                 <FormFeedback>Incorrect username or password. Please try again.</FormFeedback>
               </FormGroup>
-              <FormGroup className="w-100 m-2 text-brand">
-                <Label for="floatingInputValue2">Contraseña</Label>
-                <Input type="password" className={`form-control ${statusAuth}`} placeholder="**********" name="password" onChange={credentialHandler}/>
+              <FormGroup className="w-100 m-2 mt-3 text-brand">
+                <Label for="floatingInputValue2">Password</Label>
+                <Input type="password" className={`form-control ${statusAuth}`} placeholder="**********" name="password" onChange={credentialHandler} required />
                 <FormFeedback>Incorrect username or password. Please try again.</FormFeedback>
               </FormGroup>
-              <Button type="submit" className="m-2 mt-3 btn text-light border-0 btn-login rounded-pill w-100" onClick={authHandler} >Acceder</Button>
+              <Button type="submit" className="m-2 mt-3 btn text-light border-0 btn-login rounded-pill w-100" onClick={authHandler}>Sign In</Button>
             </Form>
           </div>
         </Col>
